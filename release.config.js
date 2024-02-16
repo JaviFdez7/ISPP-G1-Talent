b89f1d2bca11b0ef8cb6ee6c28cd331bc02eec0d
JaviFdez7 <javidelatorre400@gmail.com>
2024-02-16 11:49:39 +0100

    fix: releases workflow

--- release.config.js
+++ release.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    branches: ['main'], // Especifica tu rama de lanzamiento aquí, por ejemplo, 'main' o 'master'
+    branches: ['main', 'develop'],
     preset: 'conventionalcommits',
     ci: true,
+    repositoryUrl: 'https://github.com/JaviFdez7/ISPP-G1-Talent',
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
