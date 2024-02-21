8a219fba460892cbc44470163868b4dc74dbc314
JaviFdez7 <javidelatorre400@gmail.com>
2024-02-21 09:34:26 +0100

    feat: release workflow

--- release.config.js
+++ release.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   branches: ['main', 'develop'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/git',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
   ],
 };
