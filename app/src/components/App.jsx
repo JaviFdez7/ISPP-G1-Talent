7c2c8a05b86f224bc4a84af35940131753fd014a
carbersor <carbersor@alum.us.es>
2024-02-24 15:05:19 +0100

    fix(routes): Fix merge problems

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 //import Pricing from "../pages/Pricing";
 
 import Navbar from "./Navbar";
+import { PlaceHolder } from "../pages/PlaceHolder.jsx";
 import Login from "../pages/auth/Login.jsx";
 import Register from "../pages/auth/Register.jsx";
 
@@ -20,6 +21,16 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             {/*RUTAS PRIVADAS */}
+            {/*Analysis*/}
+            <Route path="/analysis/analyze" element={<PlaceHolder pageName="analyze" />} />
+            <Route path="/analysis/:analysisId" element={<PlaceHolder pageName="candidate analysis" />} />
+            <Route path="/analysis/history/:representativeId" element={<PlaceHolder pageName="analysis list" />} />
+            {/*Search*/}
+            <Route path="/searches/search" element={<PlaceHolder pageName="search list" />} />
+            <Route path="/searches/:searchId" element={<PlaceHolder pageName="search list" />} />
+            <Route path="/searches/history/:representativeId" element={<PlaceHolder pageName="search list" />} />
+            {/*Subscription*/}
+            <Route path="/pricing" element={<PlaceHolder pageName="subscription" />} />
             {/*<Route path="/pricing" element={<Pricing />} />*/}
           </Routes>
         </Router>
--- app/src/pages/PlaceHolder.jsx
+++ app/src/pages/PlaceHolder.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+//TODO Eliminar componente y sustituir por sus respectivos componentes funcionales
 export function PlaceHolder ( { pageName } ) {
     return (
         <div><h1>Welcome to the {pageName} page.</h1></div>
