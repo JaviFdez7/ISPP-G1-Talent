1889cfa63be0bbb44c3930da525319202643e273
marizqlav <marizqlav@alum.us.es>
2024-02-29 01:34:28 +0100

    feat: View Login and CandidateDetail

--- app/src/pages/auth/Login.jsx
+++ app/src/pages/auth/Login.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import MainButton from "../../components/mainButton.jsx";
-import SecondaryButton from "../../components/secondaryButton.jsx";
 import mainBackgroundRegisterLogin from "../../images/main-backgroundregisterlogin.jpg";
 
+import { useAuthContext } from "../../context/authContext";
+
 export default function Login() {
+  const { login } = useAuthContext();
+
   let navigate = useNavigate();
 
   //2) creamos el estado para el formulario
@@ -28,32 +31,53 @@ export default function Login() {
   //6)crear la funcion que se encargara de llamar al endpoint de login
   async function handleSubmit(e) {
     e.preventDefault();
-    const response = await fetch(
-      "https://mario.pythonanywhere.com/auth/jwt/create/",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(form),
-      }
-    );
+    const response = await fetch("http://localhost:3000/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
 
     const data = await response.json();
+    switch (response.status) {
+      case 200:
+        login(data.access, data.refresh);
+        if (data.userType === "candidate") {
+          navigate("/candidate/detail");
+        } else if (data.userType === "representative") {
+          navigate("/representative/detail");
+        }
+        break;
+      case 401:
+        setErrors(data);
+        break;
+      case 400:
+        setErrors(data);
+        break;
+      default:
+        break;
+    }
   }
 
   return (
     <div
-      className="h-screen flex flex-col justify-center bg-fixed home-container"
+      className="flex flex-col justify-center"
       style={{
+        height: "100vh",
+        backgroundAttachment: "fixed",
         backgroundImage: `url(${mainBackgroundRegisterLogin})`,
         backgroundSize: "cover",
       }}
     >
       <div
-        className="w-full max-w-3xl h-100 p-8 m-4 rounded shadow-md flex flex-col justify-between"
+        className="h-100 rounded shadow-md flex flex-col justify-between"
         style={{
           backgroundColor: "rgba(0, 0, 0, 0.5)",
+          width: "100%",
+          maxWidth: "48rem",
+          padding: "2rem",
+          margin: "1rem",
           marginLeft: "auto",
           marginRight: "auto",
           borderColor: "#d4983d",
@@ -61,64 +85,118 @@ export default function Login() {
         }}
       >
         <div>
-          <h2 className="text-6xl font-bold text-center mb-4 text-white">
+          <h2
+            className="font-bold text-center text-white"
+            style={{
+              fontSize: "4rem",
+              marginTop: "2rem",
+              marginBottom: "4rem",
+            }}
+          >
             Log in
           </h2>
           <form onSubmit={(e) => handleSubmit(e)}>
-            {/* llamada a la funcion que se encargara de hacer el login a la API*/}
-            <div className="mb-4">
+            <div
+              className="flex"
+              style={{
+                marginBottom: "1rem",
+              }}
+            >
               <label
                 htmlFor="Username"
-                className="block mb-2 text-lg font-bold text-white"
+                className="block text-lg font-bold text-white self-center"
+                style={{
+                  marginBottom: "1rem",
+                  marginRight: "2rem",
+                  marginLeft: "4rem",
+                }}
               >
                 Username
               </label>
-              <input
-                type="text"
-                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                placeholder="Write your username"
-                name="username" // nombre del atributo de la entidad del backend
-                value={username} // valor del atributo de la entidad del backend
-                onChange={(e) => onInputChange(e)} // llamada a la funcion que se encargara de actualizar el estado de la entidad
-              />
-              {/* validacion del campo del formulario */}
-              {errors.username && (
-                <p className="text-red-500 text-xs italic">{errors.username}</p>
-              )}
+              <div
+                className="flex-grow"
+                style={{
+                  marginRight: "8rem",
+                }}
+              >
+                <input
+                  type="text"
+                  className=" leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  style={{
+                    width: "100%",
+                    padding: "0.5rem 0.75rem",
+                  }}
+                  placeholder="Write your username"
+                  name="username" // nombre del atributo de la entidad del backend
+                  value={username} // valor del atributo de la entidad del backend
+                  onChange={(e) => onInputChange(e)} // llamada a la funcion que se encargara de actualizar el estado de la entidad
+                />
+                {/* validacion del campo del formulario */}
+                {errors.username && (
+                  <p className="text-red-500 text-xs italic">
+                    {errors.username}
+                  </p>
+                )}
+              </div>
             </div>
-            <div className="mb-4">
+            <div
+              className=" flex"
+              style={{
+                marginBottom: "1rem",
+              }}
+            >
               <label
                 htmlFor="Password"
-                className="block mb-2 text-lg font-bold text-white"
+                className="block text-lg font-bold text-white self-center"
+                style={{ marginRight: "2rem", marginLeft: "4rem" }}
               >
                 Password
               </label>
-              <input
-                type="password"
-                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                placeholder="Enter your password"
-                name="password"
-                value={password}
-                onChange={(e) => onInputChange(e)}
-              />
-              {errors.password && (
-                <p className="text-red-500 text-xs italic">{errors.password}</p>
-              )}
+              <div
+                className="flex-grow"
+                style={{ marginRight: "8rem", marginLeft: "0.25rem" }}
+              >
+                <input
+                  type="password"
+                  className="leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  style={{
+                    width: "100%",
+                    padding: "0.5rem 0.75rem",
+                  }}
+                  placeholder="Enter your password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => onInputChange(e)}
+                />
+                {/* validacion del campo del formulario */}
+                {errors.password && (
+                  <p className="text-red-500 text-xs italic">
+                    {errors.password}
+                  </p>
+                )}
+              </div>
             </div>
           </form>
         </div>
-        <div className="flex items-center justify-center h-full">
+        <div
+          className="flex items-center justify-center h-full"
+          style={{ marginTop: "2rem" }}
+        >
           <p className="text-md text-white mb-1 mr-2 text-center">
             Don't have an account?{" "}
             <Link
               to="/register/candidate"
-              className="text-blue-500 hover:text-blue-700 mr-2"
+              className="text-blue-500 hover:text-blue-700"
+              style={{ marginRight: "2rem" }}
             >
               Register now
             </Link>
           </p>
         </div>
-        <div className="flex justify-center items-center mt-4">
+        <div
+          className="flex justify-center items-center"
+          style={{ marginTop: "1rem" }}
+        >
           <button>{MainButton("Log in", "", "")}</button>
         </div>{" "}
         {/* MOSTRAMOS LOS ERRORES DEL ERROR 401 DE CREDENCIALES INCORRECTAS */}
--- app/src/pages/candidate/CandidateDetail.jsx
+++ app/src/pages/candidate/CandidateDetail.jsx
@@ -1,11 +1,46 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Input from "../../components/Input";
 import InputList from "../../components/InputList";
 import profile from "../../images/profile.jpg";
 import mainBackground from "../../images/main-background.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import DataTable from "../../components/DataTable.jsx";
 
 export default function CandidateDetail() {
+  const textColor2 = "#D4983D";
+
+  const [candidate, setCandidate] = useState([]);
+
+  const [user, setUser] = useState([]);
+
+  let navegate = useNavigate();
+  //2) pillamos la id de la tarea
+  const { id } = useParams();
+
+  //3) hacemos peticion a la API para obtener la tarea
+  async function getCandidateById() {
+    const response = await fetch(`http://localhost:3000/api/v1/user/${id}/`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    setUser(data);
+    setCandidate(data);
+  }
+
+  //4)llamamos a la funcion en el useEfect
+  useEffect(() => {
+    getCandidateById();
+  }, [id]);
+
+  //implementacion del metodo DELETE de la API REST para borrar una tarea
+  async function deleteCandidate() {
+    await fetch(`http://localhost:3000/api/v1/user/experience/${id}/`, {
+      method: "DELETE",
+    });
+    navegate("/");
+  }
   return (
     <div
       className="flex flex-col"
@@ -16,12 +51,12 @@ export default function CandidateDetail() {
     >
       <div className="flex flex-row justify-center">
         <div
-          className="flex flex-col items-center mt-10"
-          style={{ width: "41.6667%", padding: "4rem" }}
+          className="flex flex-col items-center"
+          style={{ width: "37%", padding: "4rem", marginTop: "5rem" }}
         >
           <img
-            src={profile}
-            className="rounded-full border border-gray-300 mb-4"
+            src={profile} //[candidate.profilePicture}
+            className="rounded-full border border-gray-300"
             style={{ width: "25vw", height: "50vh" }}
           />
         </div>
@@ -30,13 +65,91 @@ export default function CandidateDetail() {
           style={{ width: "50%", padding: "5rem", marginRight: "8rem" }}
         >
           <div className="profile-name-text">
-            <h2>Laura Martinez Ruiz</h2>
+            <h2>
+              Laura Martinez Ruiz
+              {candidate.fullName}
+            </h2>
           </div>
-          <div className="flex flex-col mt-10 w-10/12">
-            {Input("Username", "Martinaza")}
-            {Input("Email", "martinita@gmail.com")}
-            {Input("Phone number", "123456789")}
-            {Input("Ubicacion", "Sevilla, España")}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "0.5rem",
+              fontSize: "2rem",
+              color: "white",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "1rem",
+                marginBottom: "1rem",
+              }}
+            >
+              <div>Username {user.username}</div>
+              <input
+                type="text"
+                value="   Martinaza"
+                //{user.username}
+                style={{
+                  marginLeft: "2rem",
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  borderColor: "#d4983d",
+                  borderWidth: "1px",
+                }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "1rem",
+                marginBottom: "1rem",
+              }}
+            >
+              <div>Email {user.email}</div>
+              <input
+                type="text"
+                value="   martinita@gmail.com"
+                //{user.email}
+                style={{
+                  marginLeft: "2rem",
+
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  borderColor: "#d4983d",
+                  borderWidth: "1px",
+                }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "1rem",
+                marginBottom: "1rem",
+              }}
+            >
+              <div>Phone number {user.phone}</div>
+              <input
+                type="text"
+                value="   123456789"
+                //{user.phone}
+                style={{
+                  marginLeft: "1rem",
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  borderColor: "#d4983d",
+                  borderWidth: "1px",
+                }}
+              />
+            </div>
+            <div style={{ color: textColor2 }}>
+              <FontAwesomeIcon
+                icon={faMapMarkerAlt}
+                style={{ color: textColor2 }}
+              />
+              {candidate.residence} Seville, Spain
+            </div>
           </div>
         </div>
       </div>
@@ -48,20 +161,46 @@ export default function CandidateDetail() {
       <br></br>
       <div className="flex flex-row justify-center">
         <div className="flex flex-col w-5/12 p-20">
-          {InputList("Latest jobs", ["Fujitsu", "Ayesa", "..."])}
+          <DataTable
+            header={"Latest Jobs"}
+            contentArray={["TODO", "TODO", "..."]}
+          />
           <br></br>
-          {InputList("Popular repositories", ["On your tutorials", "..."])}
+          <DataTable
+            header={"Popular repositories"}
+            contentArray={["On your tutorials", "..."]}
+          />
         </div>
         <div className="flex flex-col w-5/12 p-20">
-          {Input("Preferences", "Work from home")}
+          <DataTable header={"Preferences"} contentArray={["Work from home"]} />
           <br></br>
-          {Input("Github username", "martinnez123")}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginTop: "2rem",
+              marginBottom: "1rem",
+              color: "white",
+            }}
+          >
+            <div>Github username {candidate.githubUser}</div>
+            <input
+              type="text"
+              value="   martinnez123"
+              //{candidate.githubUser}
+              style={{
+                marginLeft: "2rem",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                borderColor: "#d4983d",
+                borderWidth: "1px",
+              }}
+            />
+          </div>
           <br></br>
-          {InputList("Most popular tecnologies", [
-            "1. Java",
-            "2. Python",
-            "3. Other",
-          ])}
+          <DataTable
+            header={"Most popular tecnologies"}
+            contentArray={["1. Java", "2. Python", "3. Other"]}
+          />
         </div>
       </div>
       <h3 className="profile-title">Work experience</h3>
@@ -69,12 +208,18 @@ export default function CandidateDetail() {
       <br></br>
       <br></br>
       <br></br>
-      <div className="w-9/12 self-center">
-        {InputList("", [
-          "Fujitsu - Scraping development project",
-          "Ayesa - Main team manager on web app development",
-          "...",
-        ])}
+      <div
+        className="w-9/12 self-center"
+        style={{ marginBottom: "3rem", marginTop: "3rem" }}
+      >
+        <DataTable
+          header={""}
+          contentArray={[
+            "Fujitsu - Scraping development project",
+            "Ayesa - Main team manager on web app development",
+            "...",
+          ]}
+        />
       </div>
       <br></br>
       <br></br>
