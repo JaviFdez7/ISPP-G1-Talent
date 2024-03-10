e17db22e0e2337eb1925b44e7bf99286b9a7dda1
Andrés Domínguez Ruiz <72501425+andresdominguezruiz@users.noreply.github.com>
2024-03-10 11:49:29 +0100

    feat: E2E test and coverage

--- api/v1/tests/api/CallingApiTest.ts
+++ api/v1/tests/api/CallingApiTest.ts
@@ -1,11 +1,11 @@
 import assert from 'assert';
 const githubUsername='rwieruch'
-import { GetUserAnaliseInfo } from '../../api/v1/modules/analysis/services/GitHubService';
+import { GetUserAnaliseInfo } from '../../modules/analysis/services/GitHubService';
 
 //Estas 3 lineas son necesarias porque al estar el .env fuera de tests, no te lo detecta
 import * as dotenv from 'dotenv';
 import * as path from 'path';
-dotenv.config({ path:path.resolve(__dirname,'../../api/v1/.env') });
+dotenv.config({ path:path.resolve(__dirname,'../../.env') });
 describe(`Call the API to add a new analysis - ${githubUsername} : `, () => {
     it('Should call the API and verify that its the same user', (done: Mocha.Done) => {
         // Aquí invocas tu función que hace la llamada a la API GraphQL
--- api/v1/tests/e2e/UserControllerTest.ts
+++ api/v1/tests/e2e/UserControllerTest.ts
@@ -1,8 +1,11 @@
 import sinon from 'sinon';
+import bcrypt from 'bcryptjs';
 import { Request, Response } from 'express';
 import { EventEmitter } from 'events';
 import httpMocks from 'node-mocks-http';
 import mockRequire from 'mock-require';
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 import { loginUser } from '../../modules/user/controllers/UserController';
 import * as UserMiddleware from '../../modules/user/middlewares/UserMiddleware';
@@ -10,30 +13,55 @@ import * as UserService from '../../modules/user/services/UserService';
 import {User} from '../../modules/user/models/user';
 
 import assert from 'assert';
+import { ObjectId } from 'mongodb';
 
 
-const userMock = {
-  findOne: sinon.stub().returns({
-    exec: sinon.stub().resolves({ username: 'testUser', password: 'hashedPassword' }) // Ajusta según tus necesidades
-  })
-};
-mockRequire('../../modules/user/models/user', {userMock});
 
 
+//------DB FOR TESTING--------
+let mongoServer: MongoMemoryServer;
+before(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+});
+after(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+//------DB FOR TESTING--------
+
+const hashedPass=bcrypt.hash('fake-password', 10)
 describe('loginUser Function Tests', function() {
   let request: httpMocks.MockRequest<Request>, response: httpMocks.MockResponse<Response>;
   let checkLoginUserStub: sinon.SinonStub, loginUserStub: sinon.SinonStub;
+  let findOneStub:sinon.SinonStub;
 
   beforeEach(function() {
     // Crear request y response mocks
-    request = httpMocks.createRequest();
+    request = httpMocks.createRequest({
+      method: 'POST',
+      url: '/login',
+      // Simula lo que necesites en el cuerpo o cabeceras
+      body: {
+        username: 'fake-user',
+        password: 'fake-password'
+      }
+    });
     response = httpMocks.createResponse({
       eventEmitter: EventEmitter
     }) as httpMocks.MockResponse<Response>;
 
     // Inicializar stubs para las funciones externas
+    findOneStub = sinon.stub(User, 'findOne').withArgs({ username: 'fake-user' }).resolves({
+      username: 'fake-user',
+      password: hashedPass, // Asume que esta es la contraseña hasheada
+    });
     checkLoginUserStub = sinon.stub(UserMiddleware, 'checkLoginUser');
-    loginUserStub = sinon.stub(UserService, 'loginUser');
+    loginUserStub = sinon.stub(UserService, 'loginUser').resolves({
+      token: 'fake-token',
+      user: 'fake-user'
+    });
 
   
   });
@@ -45,16 +73,19 @@ describe('loginUser Function Tests', function() {
   });
 
   it('should return 200 and the user data on successful login', async function() {
-    const expectedData = { token: 'fake-token', user: 'fake-user' };
-    userMock.findOne.returns({ username: 'fake-user', password: 'hashedPassword' });
+    const hashedPass=await bcrypt.hash('fake-password', 10)
+    const fakeUserId = new mongoose.Types.ObjectId();
+    console.log(hashedPass)
+    findOneStub.resolves({_id:fakeUserId,username:'fake-user',password:hashedPass})
     checkLoginUserStub.resolves({});
-    loginUserStub.resolves(expectedData);
-
+    loginUserStub.resolves({ username: 'fake-user' });
+    
+    // Ejecuta la función bajo prueba
     await loginUser(request, response);
 
-    const responseData = response._getData();
     assert.strictEqual(response.statusCode, 200);
-    assert.deepStrictEqual(responseData, expectedData);
+    const responseData = response._getData();
+    assert.deepStrictEqual(responseData.user.username,'fake-user');
   });
 
 }).timeout(100000);
