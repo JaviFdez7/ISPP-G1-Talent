0e037df37fc54dee46b2742cdc1d19e9c396d4dc
RubenCasal <72756524+RubenCasal@users.noreply.github.com>
2024-03-21 15:22:39 +0100

    feat: Validator checkTeamCreatorById, checkValidToken, checkIsRepresentative, checkAuthorization

--- api/v1/modules/team-creator/routes.ts
+++ api/v1/modules/team-creator/routes.ts
@@ -8,16 +8,18 @@ import {
   deleteTeamCreator
 } from './controllers/TeamCreatorController';
 import {
-  checkCreateTeam,
+  checkIsRepresentative,
+  checkValidToken,
+  checkDataCreateTeam,
 
 } from './validators/TeamCreatorMiddleware';
 const router = express.Router();
 router.use(express.json());
 // Define routes for the TeamCreator module
 //router.get('/', getAllTeamCreator);
-router.get('/:id', getTeamCreatorById);
-router.post('/',checkCreateTeam, createTeamCreator);
+router.get('/:id',checkValidToken,checkIsRepresentative, getTeamCreatorById);
+router.post('/',checkValidToken,checkIsRepresentative,checkDataCreateTeam, createTeamCreator);
 //router.patch('/:id', updateTeamCreator);
-router.delete('/:id', deleteTeamCreator);
+router.delete('/:id',checkValidToken,checkIsRepresentative, deleteTeamCreator);
 
 export default router;
--- api/v1/modules/team-creator/validators/TeamCreatorMiddleware.ts
+++ api/v1/modules/team-creator/validators/TeamCreatorMiddleware.ts
@@ -2,14 +2,126 @@ import { verifyJWT } from '../../user/helpers/handleJWT';
 import { ApiResponse } from '../../../utils/ApiResponse';
 import { Candidate, Representative } from '../../user/models/user';
 import e, { type Request, type Response, type NextFunction } from 'express';
+import { TeamCreator } from '../models/TeamCreatorModel';
 
-export const checkCreateTeam: any = async (req: Request, res: Response, next: NextFunction) => {
+
+export const checkTeamCreatorById: any = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = req.params.id.toString();
+      const teamCreator = await TeamCreator.findById(id);
+      if (!teamCreator) {
+       
+        ApiResponse.sendError(res,[{
+            title: 'Internal Server Error',
+            detail:'TeamCreator not found'
+          }])
+          return;
+      }
+      next();
+    } catch (error: any) {
+    
+      ApiResponse.sendError(res,[{
+        title: 'Internal Server Error',
+        detail:error.message
+      }])
+      return;
+    }
+  }
+  export const checkValidToken: any = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization ?? '';
+    
+        if (token.length === 0) {
+            ApiResponse.sendError(res,[{
+                title: 'Internal Server Error',
+                detail:'No token provided.'
+              }])
+              return;
+         
+          }
+          const decodedToken = verifyJWT(token);
+        
+      next();
+    } catch (error: any) {
+    
+      ApiResponse.sendError(res,[{
+        title: 'Internal Server Error',
+        detail:error.message
+      }])
+      return;
+    }
+  }
+
+
+  export const checkIsRepresentative: any = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization ?? '';
+
+        const decodedToken = verifyJWT(token);
+        
+        const existingRepresentative = await Representative.findById(decodedToken.sub);
+        if (!existingRepresentative){
+          ApiResponse.sendError(res,[{
+              title: 'Internal Server Error',
+              detail:'Representative user not exists.'
+            }])
+        }
+      next();
+    } catch (error: any) {
+    
+      ApiResponse.sendError(res,[{
+        title: 'Internal Server Error',
+        detail:error.message
+      }])
+      return;
+    }
+  }
+
+
+  export const checkAuthorization: any = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.params.id.toString();
+        const token = req.headers.authorization ?? '';
+
+        const decodedToken = verifyJWT(token);
+        
+   
+        const representativeUser = await Representative.findById(userId);
+    if (decodedToken.sub !== representativeUser?._id.toString()) {
+   
+      ApiResponse.sendError(res,[{
+        title: 'Internal Server Error',
+        detail:'Unauthorized.'
+      }])
+      return;
+  }
+    
+      next();
+
+    } catch (error: any) {
+    
+      ApiResponse.sendError(res,[{
+        title: 'Internal Server Error',
+        detail:error.message
+      }])
+      return;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+export const checkDataCreateTeam: any = async (req: Request, res: Response, next: NextFunction) => {
     try {
     
         const data = req.body;
-     
-        const token = req.headers.authorization ?? '';
-        
         if(!data) {
           ApiResponse.sendError(res,[{
             title: 'Internal Server Error',
@@ -29,32 +141,6 @@ export const checkCreateTeam: any = async (req: Request, res: Response, next: Ne
                   return;
             }
         }
-
-        if (token.length === 0) {
-            ApiResponse.sendError(res,[{
-                title: 'Internal Server Error',
-                detail:'No token provided.'
-              }])
-              return;
-         
-          }
-      
-          const decodedToken = verifyJWT(token);
-        
-          if (decodedToken.sub !== data.userId.toString()) {
-            ApiResponse.sendError(res,[{
-                title: 'Internal Server Error',
-                detail:'Unauthorized'
-              }])
-              return;
-          }
-          const existingRepresentative = await Representative.findById(decodedToken.sub);
-          if (!existingRepresentative){
-            ApiResponse.sendError(res,[{
-                title: 'Internal Server Error',
-                detail:'Representative user not exists.'
-              }])
-          }
         
         next();
       } catch (error: any) {
