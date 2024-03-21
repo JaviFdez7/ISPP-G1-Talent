7ce9508c92401477949c1713fc8fd4e390d5acf9
Andrés Domínguez Ruiz <72501425+andresdominguezruiz@users.noreply.github.com>
2024-03-21 12:56:38 +0100

    fix: Solving dbConfig

--- api/v1/db/dbConfig.ts
+++ api/v1/db/dbConfig.ts
@@ -27,6 +27,4 @@ export async function connectToMongoDB(): Promise<void> {
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
   }
-  //In TS is usefull to add the return, even if the function return void.
-  return;
 }
