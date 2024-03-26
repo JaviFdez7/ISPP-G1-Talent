cc66eb05d8beb10b387101b434eb31695d756897
Andrés Domínguez Ruiz <72501425+andresdominguezruiz@users.noreply.github.com>
2024-03-26 11:16:21 +0100

    fix: Checks of analysis are functional

--- api/v1/modules/analysis/routes.ts
+++ api/v1/modules/analysis/routes.ts
@@ -7,14 +7,15 @@ import {
   createAnalysis,
   deleteAnalysis,
 } from './controllers/AnalysisController';
-import { validateGitHubUserAndApiKey, validateUsername, checkValidToken } from './validators/analysisvalidator'
+import { validateGitHubUserAndApiKey, validateUsername,
+   checkValidToken,checkValidTokenAndValidAnalysis,checkValidTokenAndValidGithubUser } from './validators/analysisvalidator'
 const router = express.Router();
 router.use(express.json());
 // Define routes for the Analysis module
-router.get('/'/*, checkValidToken */,getAllAnalysis);
-router.get('/:id'/*, checkValidToken */, getAnalysisById);
-router.get('/github/:username'/*, checkValidToken */, validateUsername, getAnalysisByGitHubUsername);
-router.post('/', validateGitHubUserAndApiKey/*, checkValidToken */, createAnalysis);
-router.delete('/github/:username'/*, checkValidToken */,deleteAnalysis);
+router.get('/',getAllAnalysis);
+router.get('/:id',checkValidTokenAndValidAnalysis, getAnalysisById);
+router.get('/github/:username', validateUsername, checkValidTokenAndValidGithubUser,getAnalysisByGitHubUsername);
+router.post('/', validateGitHubUserAndApiKey ,checkValidToken , createAnalysis);
+router.delete('/github/:username',checkValidTokenAndValidGithubUser,deleteAnalysis);
 
 export default router;
