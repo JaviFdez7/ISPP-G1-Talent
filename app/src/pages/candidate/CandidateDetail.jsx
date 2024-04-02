3e2c023d7ca5d07c38e7e37a52ae95838aff0973
marizqlav <marizqlav@alum.us.es>
2024-04-02 23:25:15 +0200

    feat: Added TopRepositoriesTable component and view candidate analysis data

--- app/src/pages/candidate/CandidateDetail.jsx
+++ app/src/pages/candidate/CandidateDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Input from '../../components/Input'
 import profile from '../../images/profile.jpg'
 import mainBackground from '../../images/main-background2.jpg'
@@ -9,14 +9,20 @@ import axios from 'axios'
 import { useAuthContext } from '../../context/authContext'
 import SecondaryButton from '../../components/secondaryButton'
 import WorkExperienceList from '../../components/WorkExperienceList'
-import { useNavigate } from 'react-router-dom'
-import { handleNetworkError } from '../../components/TokenExpired'
+import TopRepositoriesTable from '../../components/TopRepositoriesTable'
+
 
 export default function CandidateDetail() {
 	const { isAuthenticated } = useAuthContext()
 	const [candidate, setCandidate] = useState({})
 	const [experience, setExperience] = useState([])
-	const navigate = useNavigate()
+	const [analysis, setAnalysis] = useState(null)
+	const languages = analysis && analysis.globalTopLanguages
+		? analysis.globalTopLanguages.map((item) => item.language)
+		: []
+	const tecnologies = analysis && analysis.globalTechnologies
+		? analysis.globalTechnologies.map((item) => item)
+		: []
 
 	React.useEffect(() => {
 		const fetchUserData = async () => {
@@ -42,6 +48,29 @@ export default function CandidateDetail() {
 		}
 		fetchUserData()
 	}, [isAuthenticated])
+
+	React.useEffect(() => {
+		const fetchDataFromEndpoint = async () => {
+			try {
+				if (candidate && candidate.analysisId) {
+					const analysisId = candidate.analysisId
+					const token = localStorage.getItem('access_token')
+					const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/analysis/${analysisId}`,
+						{
+							headers: {
+								Authorization: `${token}`,
+							},
+						})
+					setAnalysis(response.data.data)
+					return response.data.data
+				}
+			} catch (error) {
+				console.error('Error fetching user data:', error)
+			}
+		}
+		fetchDataFromEndpoint()
+	}, [isAuthenticated, candidate]) 
+
 
 	return (
 		<div
@@ -79,6 +108,12 @@ export default function CandidateDetail() {
 							value: candidate ? candidate.phone : ' - ',
 							editable: false,
 						})}
+						<br></br>
+						{Input({
+							name: 'Github username',
+							value: candidate ? candidate.githubUser : ' - ',
+							editable: false,
+						})}
 						<div className='text-white mt-8'>
 							<FontAwesomeIcon
 								icon={faMapMarkerAlt}
@@ -100,39 +135,24 @@ export default function CandidateDetail() {
 			<br></br>
 			<br></br>
 			<div className='flex flex-col w-8/12 self-center'>
-				<DataTable
-					header={'Latest Jobs'}
-					contentArray={['TODO', 'TODO', '...']}
-					editable={false}
-					addLink=''
-					editLink=''
-				/>
+				<>
+					<div className='flex flex-col items-center w-8/12 self-center'>
+						<DataTable
+							header={'Top 5 Used Languages'}
+							contentArray={languages}
+						/>
+						<div className='mr-20 '></div>
+						<br></br>
+						<br></br>
+						<DataTable header={'Used Tecnologies'} contentArray={tecnologies} />
+						<br></br>
+						<br></br>
+
+						<TopRepositoriesTable analysis={analysis} />
+					</div>
+				</>
 				<br></br>
-				<br></br>
-				<DataTable
-					header={'Popular repositories'}
-					contentArray={['On your tutorials', '...']}
-				/>
-				<br></br>
-				<br></br>
-				<DataTable header={'Preferences'} contentArray={['Work from home']} />
-				<br></br>
-				<br></br>
-				<div className='w-full'>
-					{Input({ name: 'Github username', value: 'martinnez123' })}{' '}
-					{/* candidate.githubUser */}
-				</div>
-				<br></br>
-				<br></br>
-				<DataTable
-					header={'Most popular tecnologies'}
-					contentArray={['1. Java', '2. Python', '3. Other']}
-					editable={false}
-					addLink=''
-					editLink=''
-				/>
-				<br></br>
-				<br></br>
+
 			</div>
 			<h3 className='profile-title'>Work experience</h3>
 			<hr className='w-5/12 self-center'></hr>
