7ecf96ae5c74e5fd6eb9efedebc9215be9883b14
alemerpal <alemerpal@alum.us.es>
2024-04-05 18:48:25 +0200

    added modal confirmation for notifications

--- app/src/pages/candidate/CandidateNotificationDetail.jsx
+++ app/src/pages/candidate/CandidateNotificationDetail.jsx
@@ -11,15 +11,17 @@ import { useAuthContext } from '../../context/authContext'
 import MainButton from '../../components/mainButton'
 import SecondaryButton from '../../components/secondaryButton'
 import NotificationListItem from '../../components/NotificationListItem.jsx'
+import Modal from 'react-modal'
 
 export default function CandidateNotificationDetail() {
 	const [notifications, setNotifications] = useState([])
+	const [showModal, setShowModal] = useState(false)
+	const [selectedId, setSelectedId] = useState(null)
 
 	const { isAuthenticated, logout } = useAuthContext()
 
 	React.useEffect(() => {
 		const fetchNotificationsData = async () => {
-			console.log('sddsfdf')
 			try {
 				if (isAuthenticated) {
 					const currentUserId = localStorage.getItem('userId')
@@ -59,7 +61,6 @@ export default function CandidateNotificationDetail() {
 
 	const setSeenNotification = async (notificationId) => {
 		try {
-			console.log('esjdsjjkds')
 			if (isAuthenticated) {
 				const currentUserId = localStorage.getItem('userId')
 				const token = localStorage.getItem('access_token')
@@ -67,22 +68,22 @@ export default function CandidateNotificationDetail() {
 					const response = await axios.patch(
 						`${import.meta.env.VITE_BACKEND_URL}/user/${currentUserId}/notification/${notificationId}`,
 						{
-							id: notificationId,
-							userId: currentUserId,
+							id : notificationId,
+							userId : currentUserId,
 							seen: true,
 						},
 						{
 							headers: {
 								Authorization: `${token}`,
 								'Content-type': 'application/json',
-							},
+							}
 						}
 					)
 					console.log(response.data)
 				}
 			}
 		} catch (error) {
-			console.log('Error fetching notification data:', error.response.data.message)
+			console.log('Error setting notification data:', error.response.data.message)
 		}
 	}
 
@@ -113,21 +114,22 @@ export default function CandidateNotificationDetail() {
 	}
 
 	function getNotificationsList(notifications) {
-		let n1 = notifications
-			.filter((n) => !n.seen)
-			.sort((a, b) => b.dateTime.localeCompare(a.dateTime))
-			.map((n) => (
-				<NotificationListItem n={n} deleteNotificationsData={deleteNotificationsData} />
-			))
+		let n1  = notifications
+		.filter((n) => !n.seen)
+		.sort((a, b) => b.dateTime.localeCompare(a.dateTime))
+		.map((n) => (<NotificationListItem n={n} deleteNotification={deleteNotification}/>))
 
 		let n2 = notifications
 			.filter((n) => n.seen)
 			.sort((a, b) => b.dateTime.localeCompare(a.dateTime))
-			.map((n) => (
-				<NotificationListItem n={n} deleteNotificationsData={deleteNotificationsData} />
-			))
+			.map((n) => (<NotificationListItem n={n} deleteNotification={deleteNotification}/>))
 
 		return n1.concat(n2)
+	}
+
+	function deleteNotification(id) {
+		setSelectedId(id)
+		setShowModal(true)
 	}
 
 	return (
@@ -140,7 +142,7 @@ export default function CandidateNotificationDetail() {
 				backgroundSize: 'cover',
 			}}>
 			<div
-				className='h-full w-10/12 rounded shadow-md flex flex-col justify-between self-center p-4 m-4 mb-4'
+				className='h-full w-10/12 rounded shadow-md flex flex-col justify-start self-center p-4 m-4 mb-4'
 				style={{
 					backgroundColor: 'rgba(0, 0, 0, 0.5)',
 					borderColor: 'var(--talent-highlight)',
@@ -150,14 +152,69 @@ export default function CandidateNotificationDetail() {
 				<h1 className='text-5xl text-center text-white m-10'>Notifications</h1>
 
 				<div
-					className='flex flex-col justify-between items-center'
+					className='flex flex-col justify-between items-center h-max'
 					style={{ overflowY: 'scroll' }}>
 					<DataTable
 						header={''}
-						contentArray={notifications ? getNotificationsList(notifications) : []}
+						contentArray={
+							notifications
+								? getNotificationsList(notifications)
+								: []
+						}
 						editable={false}
 					/>
 				</div>
+				<Modal
+				isOpen={showModal}
+				onRequestClose={() => setShowModal(false)}
+				contentLabel='Delete Confirmation'
+				style={{
+					content: {
+						width: '40%',
+						height: '20%',
+						margin: 'auto',
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center',
+						backgroundColor: 'var(--talent-secondary)',
+						borderRadius: '10px',
+						color: 'white',
+					},
+				}}>
+				<h2 style={{ marginBottom: '3%' }}>
+					Are you sure you want to delete this professional experience?
+				</h2>
+				<div>
+					<button
+						onClick={() => {
+							deleteNotificationsData(selectedId);
+							setShowModal(false); }
+						}
+						style={{
+							marginRight: '10px',
+							padding: '10px',
+							backgroundColor: 'var(--talent-highlight)',
+							color: 'white',
+							border: 'none',
+							borderRadius: '5px',
+						}}>
+						Yes
+					</button>
+					<button
+						onClick={() => setShowModal(false)}
+						style={{
+							padding: '10px',
+							backgroundColor: 'var(--talent-black)',
+							color: 'white',
+							border: 'none',
+							borderRadius: '5px',
+						}}>
+						No
+					</button>
+				</div>
+			</Modal>
+
 			</div>
 		</div>
 	)
