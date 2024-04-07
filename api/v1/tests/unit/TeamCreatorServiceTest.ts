4e375f06213ee55dce8a7c64b5aa971a1232c144
Andrés Domínguez Ruiz <72501425+andresdominguezruiz@users.noreply.github.com>
2024-04-07 19:28:44 +0200

    feat: First team test

--- api/v1/tests/unit/TeamCreatorServiceTest.ts
+++ api/v1/tests/unit/TeamCreatorServiceTest.ts
@@ -4,6 +4,8 @@ import { createUser, getAllUser } from '../../modules/user/services/UserService'
 
 import * as dotenv from 'dotenv';
 import * as path from 'path';
+import { createProfessionalExperience,getAllProfessionalExperience} from '../../modules/professional-experience/services/ProfessionalExperienceService';
+import { createTeamCreator, getAllTeamCreatorOfRepresentative } from '../../modules/team-creator/services/TeamCreatorService';
 dotenv.config({ path: path.resolve(__dirname, '') });
 
 
@@ -14,9 +16,25 @@ dotenv.config({ path: path.resolve(__dirname, '') });
 //4º verificar que el equipo se ha creado y de que esta formado por un candidato con lo buscado
 
 describe('Create correctly a team as a representative', function() {
+    let representativeId: any
     beforeEach( async function(){
         try{
+            const representativeData= {
+                username: "representative", 
+                password: "string", 
+                email: "representative@example.com", 
+                phone: "+9720817485488", 
+                paymentMethods: [
+                    "string" 
+                ],
+                companyName: "string", 
+                companySubscription: "Basic plan", 
+                projectSocietyName: "string" 
+            };
+            const representative=await createUser(representativeData,'Representative')
+            representativeId=representative._id
             const githubUsers=['rwieruch','motero2k','RubenCasal']
+            const applications=['Web application','Backend','Frontend']
             for(let i=0;i<3;i++){
                 const userData = {
                     username: 'candidate'+i,
@@ -35,8 +53,16 @@ describe('Create correctly a team as a representative', function() {
                     lifestyle: "On-site"
                 };
                 
-                console.log(process.env.GH_TOKEN)
-                await createUser(userData,'Candidate')
+                const user=await createUser(userData,'Candidate')
+                const jobApplication = {
+                startDate: "2023-04-05T16:58:45.824+00:00", 
+                endDate: "2024-04-05T16:58:45.824+00:00", 
+                companyName: "string", 
+                professionalArea: applications[i], 
+                lifestyle: "On-site",
+                location: "string",
+                userId: user._id};
+                await createProfessionalExperience(jobApplication)
             }
         }catch(error:any){
             console.log(error)
@@ -56,13 +82,40 @@ describe('Create correctly a team as a representative', function() {
         }
     })
 
-    it('should be in db 3 new candidates', async function(){
+    //VERIFICAR SETUP
+    it('should be in db 3 new candidates+1 representative and 3 news professional experiences', async function(){
         try{
             const users=await getAllUser()
-            console.log(users)
-            assert.strictEqual(users.lenght,3)
+            assert.strictEqual(users.length,4)
+            const experiences=await getAllProfessionalExperience()
+            assert.strictEqual(experiences.length,3)
         }catch(error:any){
             console.log(error)
         }
     })
+
+    it('should create a team with one of the candidate', async function (){
+        try{
+            const teamData= [
+                {
+                    languages: [
+                        "Java" // Mapeado directamente de tu input
+                    ],
+                    technologies: [
+                        "react" 
+                    ],
+                    yearsOfExperience: 0,
+                    field: "string" 
+                }
+            ];
+            await createTeamCreator(teamData,representativeId)
+            const teams=await getAllTeamCreatorOfRepresentative(representativeId)
+            assert.strictEqual(teams.length,1)
+            assert.strictEqual(teams[0].profiles.length,1)
+        }catch(error:any){
+            console.log(error)
+        }
+        
+    })
+
 })
