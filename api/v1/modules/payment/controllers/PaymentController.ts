ecaf25417d25afc7f4c16e6a23f747fe884c5857
RubenCasal <72756524+RubenCasal@users.noreply.github.com>
2024-04-10 22:41:40 +0200

    feat: Backend Payment Gateway
    Co-authored-by: Moffinguer <fernandomateos.fermatgom@gmail.com>

---
+++ api/v1/modules/payment/controllers/PaymentController.ts
@@ -0,0 +1,72 @@
+// eslint-disable-next-line @typescript-eslint/await-thenable
+import { type Request, type Response } from 'express'
+import { ApiResponse } from '../../../utils/ApiResponse'
+import { verifyJWT } from '../../user/helpers/handleJWT'
+import { getUserById } from '../../user/services/UserService';
+import {CompanySubscription, CandidateSubscription} from '../../user/models/user';
+import { createSubscriptions } from '../../subscriptions/services/SubscriptionsService';
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+export const makePayment: any = async (req: Request, res: Response) => {
+	const subscriptionPlan = req.body.subscriptionPlan
+	const price = req.body.price
+	const payment_method = req.body.payment_method
+
+	const token = req.headers.authorization ?? ''
+	const decodedToken = verifyJWT(token)
+	const userId = decodedToken.sub
+	const user = await getUserById(userId)
+	const role = user.role
+
+	try {
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount: price * 100,
+			currency: "eur",
+			payment_method: payment_method,
+			description: `Pago por plan de suscripción ${subscriptionPlan} para el usuario ${user.username}`,
+			confirm: true,
+			automatic_payment_methods: {
+				enabled: true,
+				allow_redirects: 'never'
+			},
+			
+		  });
+			 
+		switch(paymentIntent.status) {
+			case "succeeded": {
+				const data = await createSubscriptions(role);
+				return ApiResponse.sendSuccess(res, data, 200, {
+					self: `${req.protocol}:://${req.get('host')}${req.originalUrl}`,
+				})
+		}
+		case "canceled": {
+			return ApiResponse.sendError(res, [
+				{
+					title: 'Payment cancelled',
+					detail: "An error ocurred while processing the payment, check your card permissions",
+				},
+			])
+		} 
+		default: {
+			return ApiResponse.sendError(res, [
+				{
+					title: 'Payment stopped',
+					detail: "An error ocurred while processing the payment. Aditional steps required",
+				},
+			])
+		}
+		}
+		
+	} catch (error: any) {
+		ApiResponse.sendError(res, [
+			{
+				title: 'Internal Server Error',
+				detail: error.message,
+			},
+		])
+	}
+}
+
+export default {
+	makePayment
+}
