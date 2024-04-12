6a7e7ffebe6143bbdd3bf0a5cf9b7036e0359988
carbersor <carbersor@alum.us.es>
2024-04-12 19:24:28 +0200

    feat: Add functional component for pricing tables

--- app/src/pages/candidate/CandidateSubscription.jsx
+++ app/src/pages/candidate/CandidateSubscription.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import mainBackground from "../../images/main-background2.jpg";
+import PricingTable from "../../components/subscription/PricingTable";
 
 export default function CandidateSubscription() {
   const suscriptions = [
     {
-      name: "Basic",
+      name: "Basic plan",
       price: "Free",
       description: [
         "All basic functions",
@@ -15,7 +15,7 @@ export default function CandidateSubscription() {
       ],
     },
     {
-      name: "Advance",
+      name: "Advanced plan",
       price: "9.99€",
       description: [
         "All basic functions",
@@ -37,60 +37,10 @@ export default function CandidateSubscription() {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4 justify-center">
           {suscriptions.map((suscription, index) => (
-            <div className="p-4 xl:w-1/3 md:w-1/2 w-full">
-              <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
-                <h2 className="text-sm tracking-widest title-font mb-1 font-medium text-white">
-                  {suscription.name.toUpperCase()}
-                </h2>
-                <h1 className="text-5xl pb-4 mb-4 border-b border-gray-200 leading-none text-white">
-                  {suscription.price}
-                </h1>
-                {suscription.name == "Advance" && (
-                  <span className="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
-                    POPULAR
-                  </span>
-                )}
-                {suscription.description.map((s) => (
-                  <p className="flex items-center text-white mb-2">
-                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2.5"
-                        className="w-3 h-3"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M20 6L9 17l-5-5"></path>
-                      </svg>
-                    </span>
-                    {s}
-                  </p>
-                ))}
-                <Link
-                  to={`/payments/${suscription.name}`}
-                  className={`flex items-center mt-auto border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded ${
-                    suscription.name == "standar"
-                      ? "bg-indigo-400 text-white hover:bg-indigo-600"
-                      : "text-gray-600 bg-gray-200"
-                  }`}
-                >
-                  Suscribirse
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-4 h-4 ml-auto"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </Link>
-              </div>
-            </div>
+            <PricingTable
+              suscription = {suscription}
+              key={index}
+            />
           ))}
         </div>
       </div>
--- app/src/pages/representative/RepresentativeSubscription.jsx
+++ app/src/pages/representative/RepresentativeSubscription.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import mainBackground from "../../images/main-background2.jpg";
+import PricingTable from "../../components/subscription/PricingTable";
 
 export default function RepresentativeSubscription() {
   const suscriptions = [
     {
-      name: "Basic",
+      name: "Basic plan",
       price: "29.99€",
       description: ["25 searches", "3 members per search", "Basic filters"],
     },
     {
-      name: "Advanced",
+      name: "Advanced plan",
       price: "79.99€",
       description: [
         "150 searches",
@@ -38,60 +38,10 @@ export default function RepresentativeSubscription() {
       <div className="container px-16 py-24 mx-auto">
         <div className="flex flex-wrap -m-4">
           {suscriptions.map((suscription, index) => (
-            <div className="p-4 xl:w-1/3 md:w-1/2 w-full">
-              <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
-                <h2 className="text-sm tracking-widest title-font mb-1 font-medium text-white">
-                  {suscription.name.toUpperCase()}
-                </h2>
-                <h1 className="text-5xl pb-4 mb-4 border-b border-gray-200 leading-none text-white">
-                  {suscription.price}
-                </h1>
-                {suscription.name == "Advance" && (
-                  <span className="bg-indigo-500 text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">
-                    POPULAR
-                  </span>
-                )}
-                {suscription.description.map((s) => (
-                  <p className="flex items-center text-white mb-2">
-                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                      <svg
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2.5"
-                        className="w-3 h-3"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M20 6L9 17l-5-5"></path>
-                      </svg>
-                    </span>
-                    {s}
-                  </p>
-                ))}
-                <Link
-                  to={`/payments/${suscription.name}`}
-                  className={`flex items-center mt-auto border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded ${
-                    suscription.name == "standar"
-                      ? "bg-indigo-400 text-white hover:bg-indigo-600"
-                      : "text-gray-600 bg-gray-200"
-                  }`}
-                >
-                  Suscribirse
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-4 h-4 ml-auto"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
-                </Link>
-              </div>
-            </div>
+            <PricingTable
+              suscription={suscription}
+              key={index}
+            />
           ))}
         </div>
       </div>
