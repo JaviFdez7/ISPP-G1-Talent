0225ad66a0cadad5eb74ea2e74d33eef271e5692
albdomrui <albdomrui@alum.us.es>
2024-04-13 19:45:18 +0200

    feat: added changing notifications message for different plans

--- app/src/components/NotificationListItem.jsx
+++ app/src/components/NotificationListItem.jsx
@@ -5,24 +5,25 @@ import MainButton from '../components/mainButton'
 import SecondaryButton from '../components/secondaryButton'
 
 const NotificationListItem = ({ n, deleteNotification }) => {
+	const subscription = localStorage.getItem('subscriptionType')
 	return (
-		<div className='flex flex-row'>
-			<div className='w-1/5'>{!n.seen && <p className='text-white font-bold'>New!</p>}</div>
-			<div className='w-3/5'>
-				<p className='text-white font-bold'>
-					{n.dateTime.slice(0, 10) + ' ' + n.dateTime.slice(11, 19)}
-				</p>
-				<br></br>
-				<p>{n.message}</p>
-			</div>
-			<div className='w-1/5'>
-				{MainButton('View profile', '/candidate/representative-view/' + n.representativeId)}
-			</div>
-			<div className='w-1/5'>
-				{SecondaryButton('Dismiss', '', () => deleteNotification(n._id))}
-			</div>
+	  <div className='flex flex-row'>
+		<div className='w-1/5'>{!n.seen && <p className='text-white font-bold'>New!</p>}</div>
+		<div className='w-3/5'>
+		  <p className='text-white font-bold'>
+			{n.dateTime.slice(0, 10) + ' ' + n.dateTime.slice(11, 19)}
+		  </p>
+		  <br></br>
+		  <p>{subscription === 'Basic plan' ? "A company has seen your Analysis" : n.message}</p>
 		</div>
+		<div className='w-1/5'>
+		  {subscription !== 'Basic plan' && MainButton('View profile', '/candidate/representative-view/' + n.representativeId)}
+		</div>
+		<div className='w-1/5'>
+		  {SecondaryButton('Dismiss', '', () => deleteNotification(n._id))}
+		</div>
+	  </div>
 	)
-}
-
-export default NotificationListItem
+  }
+  
+  export default NotificationListItem
