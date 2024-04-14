9b087359b8b7ec5a235decd4832928f97a880fd7
carzarrei <carzarrei@users.noreply.github.com>
2024-04-14 18:32:33 +0000

    style: Autoformat

--- api/v1/modules/subscriptions/models/subscription.ts
+++ api/v1/modules/subscriptions/models/subscription.ts
@@ -3,71 +3,71 @@ import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
 const CompanySubscriptionTypes = {
-  NO_SUBSCRIPTION: 'No subscription',
-  BASIC: 'Basic plan',
-  PRO: 'Pro plan'
+	NO_SUBSCRIPTION: 'No subscription',
+	BASIC: 'Basic plan',
+	PRO: 'Pro plan',
 }
 
 const CandidateSubscriptionTypes = {
-  BASIC: 'Basic plan',
-  PRO: 'Pro plan'
+	BASIC: 'Basic plan',
+	PRO: 'Pro plan',
 }
 
 const SupportedCurrencies = {
-  EUR: 'EUR'
-  /*
-   * USD: 'USD',
-   * GBP: 'GBP'
-   */
+	EUR: 'EUR',
+	/*
+	 * USD: 'USD',
+	 * GBP: 'GBP'
+	 */
 }
 
 const subscriptionSchema = new Schema(
-  {
-    price: {
-      amount: { type: Number, required: true },
-      currency: { type: String, required: true, enum: Object.values(SupportedCurrencies) }
-    },
-    lastPaymentDate: Date,
-    expirationDate: Date,
-    automaticRenovation: Boolean
-  },
-  { discriminatorKey: 'type' }
+	{
+		price: {
+			amount: { type: Number, required: true },
+			currency: { type: String, required: true, enum: Object.values(SupportedCurrencies) },
+		},
+		lastPaymentDate: Date,
+		expirationDate: Date,
+		automaticRenovation: Boolean,
+	},
+	{ discriminatorKey: 'type' }
 )
 
 const Subscription = model('Subscription', subscriptionSchema)
 
 const CompanySubscriptionSchema = new Schema({
-  subtype: {
-    type: String,
-    required: true,
-    enum: Object.values(CompanySubscriptionTypes)
-  },
-  remainingSearches: { type: Number, required: true, default: 0 },
-  teamLimit: { type: Number, required: true, default: 0 }
+	subtype: {
+		type: String,
+		required: true,
+		enum: Object.values(CompanySubscriptionTypes),
+	},
+	remainingSearches: { type: Number, required: true, default: 0 },
+	teamLimit: { type: Number, required: true, default: 0 },
 })
 const CompanySubscription = Subscription.discriminator(
-  'CompanySubscription',
-  CompanySubscriptionSchema
+	'CompanySubscription',
+	CompanySubscriptionSchema
 )
 
 const CandidateSubscriptionSchema = new Schema({
-  subtype: {
-    type: String,
-    required: true,
-    enum: Object.values(CandidateSubscriptionTypes)
-  },
-  remainingUpdates: { type: Number, required: true, default: 1 },
-  canInspectEmail: { type: Boolean, required: true, default: false }
+	subtype: {
+		type: String,
+		required: true,
+		enum: Object.values(CandidateSubscriptionTypes),
+	},
+	remainingUpdates: { type: Number, required: true, default: 1 },
+	canInspectEmail: { type: Boolean, required: true, default: false },
 })
 const CandidateSubscription = Subscription.discriminator(
-  'CandidateSubscription',
-  CandidateSubscriptionSchema
+	'CandidateSubscription',
+	CandidateSubscriptionSchema
 )
 
 export {
-  Subscription,
-  CompanySubscription,
-  CandidateSubscription,
-  CompanySubscriptionTypes,
-  CandidateSubscriptionTypes
+	Subscription,
+	CompanySubscription,
+	CandidateSubscription,
+	CompanySubscriptionTypes,
+	CandidateSubscriptionTypes,
 }
