dd91a82bd541a5a1cb50200f791e63248bfe29b8
alemerpal <alemerpal@alum.us.es>
2024-04-17 20:18:59 +0200

    feat: done graphs analysis view

--- app/src/components/WorkExperienceListNoButtons.jsx
+++ app/src/components/WorkExperienceListNoButtons.jsx
@@ -25,7 +25,7 @@ const WorkExperienceList = ({ experience }) => {
 			{experience.map((item, index) => (
 				<div key={index} className='flex justify-between m-2'>
 					<div
-						className='p-4 rounded-lg transition-colors duration-300 mb-2 border-b border-t mx-auto w-full'
+						className='p-4 rounded-lg transition-colors duration-300 mb-2 mx-auto w-full'
 						style={{
 							backgroundColor: 'var(--talent-dark-background)',
 							borderColor: 'var(--talent-highlight)',
@@ -36,12 +36,12 @@ const WorkExperienceList = ({ experience }) => {
 								<div
 									className={`flex ${mobile ? 'flex-col' : 'flex-row'} items-center`}>
 									<h6
-										className='text-white py-1 px-2 rounded-lg mb-2'
+										className='text-white py-1 px-2 rounded-lg'
 										style={{ backgroundColor: 'var(--talent-highlight)' }}>
 										{item.professionalArea}
 									</h6>
-									<h6 className='text-xl text-white ml-8'>{item.companyName}</h6>
-									<h6 className='text-xl text-white ml-8'>
+									<h6 className='text-white ml-8'>{item.companyName}</h6>
+									<h6 className='text-white ml-8'>
 										{formatDate(item.startDate)} - {formatDate(item.endDate)}
 									</h6>
 								</div>
