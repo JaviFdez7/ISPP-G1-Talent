e139fa0bb66bfe4dc7ef8cec527dd3ffc326c27a
albdomrui <albdomrui@alum.us.es>
2024-05-03 12:32:01 +0200

    fix: fixed favorite analysis history error

--- app/src/components/history/FavoriteButton.jsx
+++ app/src/components/history/FavoriteButton.jsx
@@ -3,32 +3,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faS, faStar } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios'
 
-const FavoriteButton = ({ history, toggleText = false }) => {
-	const [isFavorite, setIsFavorite] = useState(history.favorite)
-	const [errorMessage, setErrorMessage] = useState(null)
-	const favOn = 'var(--talent-highlight)'
-	const favOff = 'gray'
+const FavoriteButton = ({ history, isFavorite, onToggleFavorite, toggleText = false }) => {
+    const [errorMessage, setErrorMessage] = useState(null)
+    const favOn = 'var(--talent-highlight)'
+    const favOff = 'gray'
 
-	const apiURL = import.meta.env.VITE_BACKEND_URL
+    const apiURL = import.meta.env.VITE_BACKEND_URL
 
-	async function changeFavorites(userId) {
-		const uri = `/user/${userId}/history/${history._id}/favorite`
-		try {
-			const response = await axios.patch(apiURL + uri)
-			setErrorMessage(null)
-			return response
-		} catch (error) {
-			setErrorMessage('Unable to connect to the server. Please try again later.')
-			console.error('Error changing history favorite state:', error)
-			throw error
-		}
-	}
+    async function changeFavorites(userId) {
+        const uri = `/user/${userId}/history/${history._id}/favorite`
+        try {
+            const response = await axios.patch(apiURL + uri)
+            setErrorMessage(null)
+            return response
+        } catch (error) {
+            setErrorMessage('Unable to connect to the server. Please try again later.')
+            console.error('Error changing history favorite state:', error)
+            throw error
+        }
+    }
 
-	const handleToggleFavorite = () => {
-		setIsFavorite(!isFavorite)
-		const userId = localStorage.getItem('userId')
-		changeFavorites(userId)
-	}
+    const handleToggleFavorite = () => {
+        onToggleFavorite()
+        const userId = localStorage.getItem('userId')
+        changeFavorites(userId)
+    }
 
 	return (
 		<button
