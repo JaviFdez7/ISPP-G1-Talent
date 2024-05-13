dd05e781859b649ad30abe8f19fc57daeb533dda
Daniel Gallardo Martos <dangalmar@alum.us.es>
2024-05-13 21:05:36 +0200

    fix: Fixed number of candidates in search

--- app/src/pages/search/SearchForm.jsx
+++ app/src/pages/search/SearchForm.jsx
@@ -332,11 +332,13 @@ export default function SearchForm() {
 		if (subscription) {
 			if (subscription.toLowerCase() == 'basic plan') {
 				setNumOptions([
+					{ value: 1, label: '1' },
 					{ value: 2, label: '2' },
 					{ value: 3, label: '3' },
 				])
 			} else {
 				setNumOptions([
+					{ value: 1, label: '1' },
 					{ value: 2, label: '2' },
 					{ value: 3, label: '3' },
 					{ value: 4, label: '4' },
